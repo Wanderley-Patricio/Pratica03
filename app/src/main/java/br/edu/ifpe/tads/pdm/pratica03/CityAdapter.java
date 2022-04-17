d4b22c5b8492531e24b39a6fd38491aaaaa13dd1
Wanderley-Patricio <86197936+Wanderley-Patricio@users.noreply.github.com>
2022-04-17 15:01:47 -0300

    Prat-03 - ListView - Parte 4

--- app/src/main/java/br/edu/ifpe/tads/pdm/pratica03/CityAdapter.java
+++ app/src/main/java/br/edu/ifpe/tads/pdm/pratica03/CityAdapter.java
@@ -17,12 +17,20 @@ public class CityAdapter extends ArrayAdapter<City> {
 
     @Override
     public View getView(int position, View view, ViewGroup parent) {
-        LayoutInflater inflater = LayoutInflater.from(getContext());
-        View listItem = inflater.inflate(R.layout.city_listitem, null, true);
+        View listItem = null;
+        if (view == null) {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            listItem = inflater.inflate(R.layout.city_listitem, null, true);
+        } else {
+            listItem = view;
+        }
+
         TextView cityName = listItem.findViewById(R.id.city_name);
         TextView cityInfo = listItem.findViewById(R.id.city_info);
+
         cityName.setText(cities[position].getName());
         cityInfo.setText(cities[position].getInfo());
+
         return listItem;
     }
 }
