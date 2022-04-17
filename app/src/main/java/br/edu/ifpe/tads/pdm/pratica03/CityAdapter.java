79b0960dc38a366583c24675df6f13f4606fa193
Wanderley-Patricio <86197936+Wanderley-Patricio@users.noreply.github.com>
2022-04-17 15:10:43 -0300

    Prat-03 - ListView - Parte 5

--- app/src/main/java/br/edu/ifpe/tads/pdm/pratica03/CityAdapter.java
+++ app/src/main/java/br/edu/ifpe/tads/pdm/pratica03/CityAdapter.java
@@ -7,7 +7,16 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+
+
 public class CityAdapter extends ArrayAdapter<City> {
+
+    //classe estática ViewHolder dentro de CityAdapter
+    static class ViewHolder {
+        TextView cityName;
+        TextView cityInfo;
+    }
+
     private City[] cities;
 
     public CityAdapter(Context context, int resource, City[] cities) {
@@ -18,18 +27,24 @@ public class CityAdapter extends ArrayAdapter<City> {
     @Override
     public View getView(int position, View view, ViewGroup parent) {
         View listItem = null;
+        ViewHolder holder = null;
+
         if (view == null) {
             LayoutInflater inflater = LayoutInflater.from(getContext());
             listItem = inflater.inflate(R.layout.city_listitem, null, true);
+
+            holder = new ViewHolder();
+            holder.cityName = listItem.findViewById(R.id.city_name);
+            holder.cityInfo = listItem.findViewById(R.id.city_info);
+
+            listItem.setTag(holder);
         } else {
             listItem = view;
+            holder = (ViewHolder)view.getTag();
         }
 
-        TextView cityName = listItem.findViewById(R.id.city_name);
-        TextView cityInfo = listItem.findViewById(R.id.city_info);
-
-        cityName.setText(cities[position].getName());
-        cityInfo.setText(cities[position].getInfo());
+        holder.cityName.setText(cities[position].getName());
+        holder.cityInfo.setText(cities[position].getInfo());
 
         return listItem;
     }
